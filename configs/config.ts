f5ac3adadf50f6251bb957ddfd978e47249adb6e
Oleksandr Skvortsov <skvortsov2001@gmail.com>
2025-04-06 23:19:48 +0300

    add envs in CI

--- configs/config.ts
+++ configs/config.ts
@@ -1,13 +1,17 @@
 
 export const configDev = {
-    baseURL : "https://mriia-dev-webapp.azurewebsites.net/",
+    baseURL :  process.env.ENVIRONMENT === 'Dev' 
+          ? 'https://mriia-stage-webapp.azurewebsites.net' 
+          : "https://mriia-dev-webapp.azurewebsites.net",
     login: "m.zhuravel@thedigital.gov.ua",
     password: "Aa123456!",
-    lessonForNewMaterialDev: "https://mriia-dev-webapp.azurewebsites.net/Timetable/LessonEditView?lessonID=76759&lessonType=1&refferal=2#"
 }
 
 export const configStage = {
-    baseURL : "https://mriia-stage-webapp.azurewebsites.net/Welcome",
+    baseURL :  process.env.ENVIRONMENT === 'Stage' 
+          ? 'https://mriia-stage-webapp.azurewebsites.net' 
+          : "https://mriia-dev-webapp.azurewebsites.net",
+      
     keyFilePath: '/Users/oleksandrskvorcov/Desktop/stage_certs_admin_2025/Фізична особа (з РНОКПП)/Key-6.dat',
     password: "12345",
     lessonForNewMaterialStage: ""
