53785a0ceb6ed7e59ca5b67e6dc0646b03bd3774
Oleksandr Skvortsov <skvortsov2001@gmail.com>
2025-04-09 17:39:21 +0300

    Change test titles

--- tests/sidebar-menu.spec.ts
+++ tests/sidebar-menu.spec.ts
@@ -10,37 +10,37 @@ test.describe('Навігація по меню', () => {
     await page.goto('/');
   });
 
-  test('Перехід до розділу Школи', async ({ page }) => {
+  test('Перевірка завантаження розділу Школи', async ({ page }) => {
     await menu.openSchools();
     await expect(page.getByRole('link', { name: 'Сервери' })).toBeVisible();
   });
 
-  test('Перехід до розділу Служба підтримки', async ({ page }) => {
+  test('Перевірка завантаження розділу Служба підтримки', async ({ page }) => {
     await menu.openSupportService();
     await expect(page.getByLabel('breadcrumbs').getByText('Служба підтримки')).toBeVisible();
   });
 
-  test('Перехід до розділу Новини платформи', async ({ page }) => {
+  test('Перевірка завантаження розділу Новини платформи', async ({ page }) => {
     await menu.openPlatformNews();
     await expect(page.locator('text=Додати')).toBeVisible();
   });
 
-  test('Перехід до розділу Користувачі', async ({ page }) => {
+  test('Перевірка завантаження розділу Користувачі', async ({ page }) => {
     await menu.openUsers();
     await expect(page.locator('text=Керування користувачами')).toBeVisible();
   });
 
-  test('Перехід до розділу Шаблони документів', async ({ page }) => {
+  test('Перевірка завантаження розділу Шаблони документів', async ({ page }) => {
     await menu.openDocumentTemplates();
     await expect(page.locator('text=файл')).toBeVisible();
   });
 
-  test('Перехід до розділу Документи школи', async ({ page }) => {
+  test('Перевірка завантаження розділу Документи школи', async ({ page }) => {
     await menu.openSchoolDocuments();
     await expect(page.locator('text=Номер договору')).toBeVisible();
   });
 
-  test("Перехід до розділу Головна сторінка", async({page})=>{
+  test("Перевірка завантаження розділу Головна сторінка", async({page})=>{
     await menu.openMainPage()
     await expect(page.locator('#message')).toBeVisible();
   })
